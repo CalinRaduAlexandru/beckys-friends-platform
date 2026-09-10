3a3ba57b2c6195cee583dba317b70cc51b080d93
Radu <raducalinalexandru@gmail.com>
2026-09-10 22:20:10 +0300

    Add scannable party regulations page

--- src/worker.mjs
+++ src/worker.mjs
@@ -20,6 +20,8 @@ const ROUTES = new Map([
   ['/music-for-kids/parent', '/music-for-kids/parent.html'],
   ['/p', '/assets/Pontaj_Echipa_Septembrie_2026_2_pagini_luni_normale.pdf'],
   ['/petreceri', '/petreceri.html'],
+  ['/regulament-petreceri', '/regulament-petreceri.html'],
+  ['/regulament-petreceri/', '/regulament-petreceri.html'],
   ['/invitatii-petrecere', '/invitatii-petrecere.html'],
   ['/invitatii-petrecere/', '/invitatii-petrecere.html'],
   ['/evenimente', '/evenimente.html'],
